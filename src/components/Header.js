184d26dd8e41ba00a3497998b64cb6b6d030b49f
SilentTush <tkushwaha70@gmail.com>
2021-06-22 22:56:23 +0530

    finished project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-function Header({ heading }) {
+function Header({
+  heading,
+  loadHistoricData,
+  historicData,
+  closeHistoricData,
+}) {
+  const location = useLocation();
+  const history = useHistory();
   return (
     <div className="header">
-      <h2>{heading}</h2>
+      <h2 onClick={() => history.push("/")}>{heading}</h2>
+      {location.pathname !== "/" ? (
+        <div
+          className="historicButton"
+          onClick={historicData ? closeHistoricData : loadHistoricData}
+        >
+          {`${historicData ? "Close" : "Get"} Historic Data`}
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,25 +1,93 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Header from "../components/Header";
 
 function CountryPage() {
   const { countryName } = useParams();
   const [data, setData] = useState([]);
+  const [historicData, setHistoricData] = useState(null);
+  const history = useHistory();
+  async function loadHistoricData() {
+    axios
+      .get(`https://corona.lmao.ninja/v2/historical/${countryName}`)
+      .then((result) => {
+        setHistoricData(result.data.timeline);
+      })
+      .catch((err) => {
+        alert(`Historic data for ${countryName}, does not exist`);
+      });
+  }
   useEffect(() => {
     getData();
     async function getData() {
       axios
         .get(`https://corona.lmao.ninja/v2/countries/${countryName}`)
         .then((result) => {
+          let data = result.data;
+          delete data.undefined;
           setData(result.data);
+        })
+        .catch((err) => {
+          history.push("/");
+          alert(`Cannot get Data for ${countryName}`);
         });
     }
   }, []);
 
   return (
     <div className="countryPage">
-      <Header heading={countryName[0].toUpperCase() + countryName.slice(1)} />
+      <Header
+        heading={countryName[0].toUpperCase() + countryName.slice(1)}
+        loadHistoricData={loadHistoricData}
+        historicData={historicData}
+        closeHistoricData={() => setHistoricData(null)}
+      />
+      <img src={data?.countryInfo?.flag} />
+      {historicData === null ? (
+        <div className="detailsWrapper">
+          {Object.keys(data).map((item, index) => {
+            if (item === "countryInfo" || item === "updated") return null;
+            else
+              return (
+                <div
+                  className={`dataItem ${index % 2 === 0 ? "" : "alternate"} `}
+                >
+                  <p className="title">
+                    {item[0].toUpperCase() + item.slice(1)}
+                  </p>
+                  <p className="data">{data[item]}</p>
+                </div>
+              );
+          })}
+        </div>
+      ) : (
+        <div className="historicWrapper">
+          <div className="dataItem heading">
+            <p className="cell date">Date</p>
+            <p className="cell cases">Cases</p>
+            <p className="cell deaths">Deaths</p>
+            <p className="cell recovered">Recovered</p>
+          </div>
+          {Object.keys(historicData.cases).map((date, index) => {
+            let indianDate =
+              date.split("/")[1] +
+              "-" +
+              date.split("/")[0] +
+              "-" +
+              date.split("/")[2];
+
+            return (
+              <div className={`dataItem ${index % 2 === 0 ? "" : "alternate"}`}>
+                <p className="cell date">{indianDate}</p>
+                <p className="cell cases">{historicData.cases[date]}</p>
+                <p className="cell deaths">{historicData.deaths[date]}</p>
+                <p className="cell recovered">{historicData.recovered[date]}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
